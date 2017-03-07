2242360adf9f6d0a5949174ad14fff9d3b36dcec
Pedro Jordano <jordano@ebd.csic.es>
2017-03-07 18:42:44 +0100

    Added data example file.

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Visitation and fruit consumption data for Prunus mahaleb.
+#'
+#' A dataset containing visitation and fruit handling data for Prunus mahaleb trees studied in SE Spain. Jordano and Schupp 2000 Ecological Monographs. 
+#'
+#' @format A data frame with 25 rows and 11 variables:
+#' \describe{
+#'   \item{dataset}{Data identifier.}
+#'   \item{plant}{Plant species identifier.}
+#'   \item{animal}{Animal species identifier.}
+#'   \item{group}{Grouping category for species.}
+#'   \item{abundance}{Local abundance (no./10ha).}
+#'   \item{visits}{Visitation rate (no. visits/10h),.}
+#'   \item{prop_visits}{Proportion of all visits recorded.}
+#'   \item{eff_per_vis}{Effectiveness per visit.}
+#'   \item{eff_total}{Total effectiveness.}
+#'   \item{prop_disp_service}{Proportion of total effectiveness.}
+#'   \item{frugivore_species}{Animal species name.}
+#' }
+#' @source \url{https://raw.githubusercontent.com/pedroj/effectiveness/master/data.txt}
+“prunus”
