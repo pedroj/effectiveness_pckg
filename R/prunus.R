5964502a412485b7d3683b2a8c36bf369f37736d
Pakillo <f.rodriguez.sanc@gmail.com>
2017-03-08 15:49:39 +0100

    roxygen for prunus dataset

---
+++ R/prunus.R
@@ -0,0 +1,33 @@
+#' Quantitative component of effectiveness for Prunus mahaleb-frugivorous birds interactions.
+#'
+#' Data from a study on Prunus mahaleb (Rosaceae) seed dispersal by frugivorous
+#' animals in SE Spain.
+#' Variables include:
+#' # Visitation data come from 107.3 h direct watches.
+#' abundance-          Mean no. birds censused/km, averaged for two study years.
+#' visits-             Mean no. visitis recorded to fruiting trees (/10 h).
+#' prop_visits-        Proportion of total visits recorded (feeding records)
+#'                     contributed by species. Relative to the total no. 
+#'                     records in two study years.
+#' eff_per_vis-        Mean no. fruits swallowed per visit (successfully 
+#'                     dispersed seeds).
+#' eff_total-          Visit rate * eff_per_vis*prop fruits swallowed.
+#' prop_disp_service-  Proportion of total dispersal service contributed by
+#'                     species.
+#'
+#' @docType data
+#' 
+#' @name prunus
+#' 
+#'
+#' @usage data(prunus)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Schupp, E. W., Jordano, P. and Gomez, J.M. 2010. Seed dispersal effectiveness revisited: a conceptual review. New Phytologist 188: 333-353.
+#'
+#' @source \href{https://raw.githubusercontent.com/pedroj/effectiveness/master/data.txt}{Data txt archive}
+#'
+NULL
