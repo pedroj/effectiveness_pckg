588f47455519df8463bb5ba5da4bad2c3c3a985d
Pedro Jordano <jordano@ebd.csic.es>
2017-03-10 13:06:31 +0100

    Corrected errors in Rd files from last Rproj update.

--- R/effectiveness.R
+++ R/effectiveness.R
@@ -57,7 +57,7 @@ effectiveness<- function(q1, q2, group=NA, label= NA, nlines=10,
     # Main plot ------------------------------------------------------------
     # mytheme_bw.R
     # devtools::source_gist("https://gist.github.com/b843fbafa3af8f408972")
-    devtools::source_gist("b843fbafa3af8f408972", filename = "mytheme_bw.R")
+    source("./R/mytheme_bw.R")
     #
     p1<- ggplot(d, aes(x= q1, y= q2)) + 
         geom_point(shape= group, size= 3) +
