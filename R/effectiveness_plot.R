208be81757288f0e44b52f4a349aea5d6c619d05
Pakillo <f.rodriguez.sanc@gmail.com>
2017-07-18 21:17:12 +0200

    Enable using italic fonts for point labels

--- R/effectiveness_plot.R
+++ R/effectiveness_plot.R
@@ -13,6 +13,7 @@
 #' @param pts.color Optional. A grouping variable to set point colours (e.g., family).
 #' @param pts.size Optional. Size of points.
 #' @param label Optional. A character vector of the same length as \code{q1} and \code{q2} providing a label for the individual points (e.g., species acronym). Note that \code{label} may be NA for some points (useful to avoid overplotting of labels).
+#' @param italic Logical. Use italic font for labels? 
 #' @param show.lines Logical. Show effectiveness isolines? (default is TRUE).
 #' @param nlines Specify the number of isolines.
 #' @param lines.breaks Either a numeric vector giving break points for the contour lines, or a \code{"style"} (e.g. \code{"quantile"}, \code{"equal"}, or \code{"pretty"}) to choose optimal breaks using \code{\link[classInt]{classIntervals}}. Note that using "pretty" will override \code{nlines}, as the number of lines will be determined algorithmically. See \code{\link[classInt]{classIntervals}} for more details.
@@ -61,7 +62,7 @@
 effectiveness_plot <- function(q1, q2, 
                                q1.error = NULL, q2.error = NULL, 
                                pts.shape = NULL, pts.color = NULL, pts.size = 2,
-                               label = NA, 
+                               label = NA, italic = FALSE, 
                                show.lines = TRUE, nlines = 6,
                                lines.breaks = "quantile", lines.color = "grey50", 
                                myxlab= "QtComp", myylab= "QltComp")    {
@@ -195,7 +196,8 @@ effectiveness_plot <- function(q1, q2,
         effplot <- effplot +
             geom_text_repel(aes(x, y), data = d, size = 3, label = label, 
                             nudge_y = 0.5, 
-                            segment.size = 0.2, segment.alpha = 0.75) 
+                            segment.size = 0.2, segment.alpha = 0.75,
+                            fontface = ifelse(isTRUE(italic), "italic", "plain")) 
     }
     
     
