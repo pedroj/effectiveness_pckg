efa740fd6e945e9bc47dd4259316a0a8c93788a1
pedrojordano <pedroj@me.com>
2026-08-10 00:43:30 +0200

    Added Euphonia data.

---
+++ R/euphonia.R
@@ -0,0 +1,35 @@
+#' Quantitative component of RPE effectiveness for Euphonia species.
+#'
+#' Variables include:
+#'
+#' Foraging data of Euphonia species on different fruit species.
+#' species-            Euphonia species.
+#'
+#' plant-              Plant species.
+#'
+#' plant_family-       Plant family.
+#'
+#' QTY-                Quantity component. Mean no. fruits swallowed per visit 
+#'                     (successfully dispersed seeds).
+#'
+#' QTY_SD-             Quantity component standard deviation.
+#'
+#' QTY_SE-             Quantity component standard error.
+#'
+#'energy_fruit-        Energy yield/fruit consumed.
+#'
+#'TE-                  Total effectiveness.
+#'
+#' @docType data
+#' 
+#' @usage data(euphonia)
+#'
+#' @format A dataset (dataframe).
+#'
+#' @keywords datasets
+#'
+#' @references Quintero, E., M. A. Pizo, and P. Jordano. 2020. Fruit resource provisioning for avian frugivores: The overlooked side of effectiveness in seed dispersal mutualisms. Journal of Ecology 108:1358–1372.
+#'
+#' @source \href{https://raw.githubusercontent.com/pedroj/effectiveness/data-raw/euphonia.txt}{Data txt archive}
+#'
+"euphonia"
